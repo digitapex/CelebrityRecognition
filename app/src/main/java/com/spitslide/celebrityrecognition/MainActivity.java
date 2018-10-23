9525d500c0dda6a683982b64de065dceeafe5385
digitapex <spitslide@gmail.com>
2018-10-23 22:04:39 +0200

    Add tap to take photo

--- app/src/main/java/com/spitslide/celebrityrecognition/MainActivity.java
+++ app/src/main/java/com/spitslide/celebrityrecognition/MainActivity.java
@@ -1,13 +1,41 @@
 package com.spitslide.celebrityrecognition;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String IMAGE = "image";
+    private static final java.lang.String EXT = ".jpg";
+    private static final int REQUEST_TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void takePhoto(View view) throws IOException {
+        File cacheDir = getCacheDir();
+        File image = File.createTempFile(IMAGE, EXT, cacheDir);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            Uri photoURI = Uri.fromFile(image);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
     }
 }
